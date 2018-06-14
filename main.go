834243546c04e10750c8a8202df5a6e1124b2fb8
Dylan Graham <dylan.graham@gmail.com>
2018-06-14 17:08:16 +1000

    test data not needed anymore

--- main.go
+++ main.go
@@ -9,60 +9,6 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-var data = `
-apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: LS0tLS1CRUdJTiBJQ0FURS0tLS0tCg==
-    server: https://api-blah.ap-southeast-2.elb.amazonaws.com
-  name: blah.k8s.local
-- cluster:
-    certificate-authority-data: LS0tLS1CRUdJTiBGSUNBVEUtLS0tLQo=
-    server: https://api-blah-blah.ap-southeast-2.elb.amazonaws.com
-  name: blah.blah.k8s.local
-contexts:
-- context:
-    cluster: blah.k8s.local
-    user: blah.k8s.local
-  name: blah.k8s.local
-- context:
-    cluster: blah.blah.k8s.local
-    namespace: default
-    user: blah.blah.k8s.local
-  name: blah.blah.k8s.local
-current-context: blah.k8s.local
-kind: Config
-preferences: {}
-users:
-- name: blah.k8s.local
-  user:
-    client-certificate-data: LS0tLS1CRjlzeDVwck9yTT0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=
-    client-key-data: LS0tLS1CRUdJTiBSUFJJVkFURSBLRVktLS0tLQo=
-    password: MWt1wbazqUWFOdeXcsITuE
-    username: admin
-- name: blah.k8s.local-basic-auth
-  user:
-    password: MWt1wbazqUWFOdeXcsITuE
-    username: admin
-- name: blah.blah.k8s.local
-  user:
-    client-certificate-data: LS0tLS1CRUdwV1Y3bytMUT0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=
-    client-key-data: LS0tLS1CRUdJTiBSU0EJVkFURSBLRVktLS0tLQo=
-    password: xCxHeFZ4bCZ1HfPl6VsaSx
-    username: admin
-- name: blah.blah.k8s.local-basic-auth
-  user:
-    password: xCxH2TZ4bCZ1HfPl6VsaSx
-    username: admin
-- name: developer/127-0-0-1:8443
-  user:
-    token: t92DXuXe8zkHyFpFgNGkeWI
-- name: system:admin/127-0-0-1:8443
-  user:
-    client-certificate-data: LS0tLSRPT0s1YmM9Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K
-    client-key-data: LS0tLS1CRCBSU0EgUFJJVkFURSBLRVktLS0tLQo=
-`
-
 type Cluster struct {
 	CertificateAuthorityData string `yaml:"certificate-authority-data"`
 	Server                   string `yaml:"server"`
